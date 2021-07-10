8b4109f56c0a99ee7e0068ff350b0c16759fa5ee
Clayton <clayton.almeida.campos@gmail.com>
2021-07-10 14:01:14 -0300

    created style global

---
+++ src/styles/global.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle` 
+
+:root{
+    --background: #FFF;
+    --container: 77.5rem //1240px
+}
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+
+html, body {
+    height: 100vh;
+    background: var(--background);
+}
+
+body, input, text-area, button {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+  }
+
+h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+}
+
+
+button {
+    cursor: pointer;
+  }
+
+`;
